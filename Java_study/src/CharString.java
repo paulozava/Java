afd522c91ac4a57288a7c16f663278aa3420fc00
paulozava <camposzava@gmail.com>
2018-08-22 11:03:06 +0200

    char and String

---
+++ Java_study/src/CharString.java
@@ -0,0 +1,15 @@
+
+public class CharString {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		char b = 66;
+		System.out.println(a);
+		System.out.println(b);
+		
+		String c = "a";
+		String d = "66";
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
