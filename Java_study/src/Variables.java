1a693b2a156d80f90e518e37465548b1521482d9
paulozava <camposzava@gmail.com>
2018-08-22 10:23:36 +0200

    Basic variable declarations

---
+++ Java_study/src/Variables.java
@@ -0,0 +1,16 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int integerOne = 1;
+		int integerTwo = 2;
+		double doubleOne = 1.0;
+		double doubleTwo = 2;
+		
+		System.out.println(integerOne / integerTwo);
+		System.out.println(doubleOne / doubleTwo);
+		System.out.println(integerOne / doubleOne);
+
+	}
+
+}
