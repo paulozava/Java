dfd7693d88f353bc8d579626c85ac143961d4ef9
paulozava <camposzava@gmail.com>
2018-08-22 15:55:38 +0200

    add instace of Conta

--- ByteBank/src/Conta/Conta.java
+++ ByteBank/src/Conta/Conta.java
@@ -5,4 +5,5 @@ public class Conta {
 	int agencia;
 	int conta;
 	String titular;
+
 }
---
+++ ByteBank/src/Conta/IstanciaConta.java
@@ -0,0 +1,9 @@
+package Conta;
+
+public class IstanciaConta {
+	public static void main(String[] args) {
+		Conta novaConta = new Conta();
+		novaConta.saldo = 0.0;
+		System.out.println(novaConta.saldo);
+	}
+}
