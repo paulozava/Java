7106a1abbd810fa73c491e8c6dd878e65d6c35c0
paulozava <camposzava@gmail.com>
2018-08-23 08:00:39 +0200

    add cliente specs class

---
+++ ByteBank/src/Cliente/Cliente.java
@@ -0,0 +1,8 @@
+package Cliente;
+
+public class Cliente {
+    String nome;
+    String cpf;
+    int idade;
+    Endereco enderecoCliente;
+}
---
+++ ByteBank/src/Cliente/Endereco.java
@@ -0,0 +1,10 @@
+package Cliente;
+
+public class Endereco {
+    String logradouro;
+    String complemento;
+    String numero;
+    String bairro;
+    String cidade;
+    String cep;
+}
