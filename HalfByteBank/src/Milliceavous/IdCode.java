ff78962243632820df567eb6e5209d65a5af1cb8
paulozava <camposzava@gmail.com>
2018-08-27 14:46:59 +0200

    add basic template to create internal id

---
+++ HalfByteBank/src/Milliceavous/IdCode.java
@@ -0,0 +1,14 @@
+package Milliceavous;
+
+public class IdCode {
+
+	public IdCode() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public long getId() {
+		return 0L;
+		// TODO Auto-generated method stub
+
+	}
+}
