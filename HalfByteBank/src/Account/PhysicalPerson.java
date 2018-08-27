1267fb0ca92ed9035047d790e0c1a7d9e98f76bf
paulozava <camposzava@gmail.com>
2018-08-27 14:44:54 +0200

    Add physical person class initiator

--- HalfByteBank/src/Account/PhysicalPerson.java
+++ HalfByteBank/src/Account/PhysicalPerson.java
@@ -2,11 +2,14 @@ package Account;
 
 public class PhysicalPerson extends BasicAccount{
 
+	
+	
 	public PhysicalPerson(int accountNumber, int bankAgency) {
 		this.setAccountNumber(accountNumber); 
 		this.setVerificationOfNumber(accountNumber); 
 		this.setBankAgency(bankAgency);
 		this.setVerificationOfAgency(bankAgency);
 		this.setBalance(0.0);
+
 	}
 }
